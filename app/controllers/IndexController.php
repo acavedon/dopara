f84d66f6d4de4c203a9802230e06493828616cca
Alexandre Cavedon <alexandre.cavedon@nozebra.com.br>
2011-08-05 17:21:11 -0300

    Fixed bootstrap

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,8 +2,33 @@
 class IndexController extends Zend_Controller_Action {
 
   public function indexAction() {
+
+    /*
+    $params = array('host' => 'localhost',
+                    'username' =>'root',
+                    'password' =>'',
+                    'dbname' =>'test');
+    include("Zend/Db/Adapter/Pdo/Mysql.php");
+    $DB = new Zend_Db_Adapter_Pdo_Mysql($params);
+    $DB->setFetchMode(Zend_Db::FETCH_OBJ);
+
+    $sql = "SELECT * FROM `auth`";
+    $result = $DB->fetchAssoc($sql);
+
+print_r($result);
+    */
+
     $this->view->title = 'Title Test';
     $this->view->body = 'This is a little test';
+  }
+
+  public function listAction() {
+
+    /*
+    $this->view->assign('title','Member List');
+    $this->view->assign('description','Below, our members:');
+    $this->view->assign('datas',$result);
+     */
   }
 }
 ?>
--- index.php
+++ index.php
@@ -3,20 +3,17 @@ error_reporting(E_ALL|E_STRICT);
 setlocale (LC_ALL, 'pt_BR');
 date_default_timezone_set('America/Sao_Paulo');
 
-// seta os path e faz os requires
 ini_set('include_path',
-   '.'.PATH_SEPARATOR.'./lib'
+      get_include_path()
+      .PATH_SEPARATOR.'./lib'
       .PATH_SEPARATOR.'./app/models/'
-      .PATH_SEPARATOR.get_include_path);
+ );
 
 include "Zend/Loader.php";
 Zend_Loader::loadClass('Zend_Controller_Front');
 
-// abre o front controller (detecta a base url e redireciona)
-$controlador = Zend_Controller_Front::getInstance();
-$controlador->throwExceptions(true);
-$controlador->setControllerDirectory('./app/controllers'); // seta diretório com nossos controllers
-
-// roda o sistema
-$controlador->dispatch();
+$controller = Zend_Controller_Front::getInstance();
+$controller->throwExceptions(true);
+$controller->setControllerDirectory('./app/controllers');
+$controller->dispatch();
 ?>
